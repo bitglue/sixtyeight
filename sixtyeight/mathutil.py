4bfb779c22c9181ed108714cd4142e3fff6b16ad
Phil Frost <indigo@bitglue.com>
2011-05-30 09:58:58 -0400

    add a simple IIR filter. Not sure if it is useful yet.

--- sixtyeight/mathutil.py
+++ sixtyeight/mathutil.py
@@ -1,3 +1,7 @@
+from __future__ import division
+from math import exp, pi
+
+
 def movingSum(x, n):
     if len(x) < n:
         return []
@@ -19,3 +23,32 @@ def movingSum(x, n):
         tail = x[i-n+2]
 
     return result
+
+
+class IIR(object):
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def filter(self, x):
+        def getitem(seq, i):
+            if i < 0:
+                return 0
+            return seq[i]
+
+        y = []
+
+        for i in xrange(len(x)):
+            nextValue = 0
+            for j, a in enumerate(self.a):
+                nextValue += a * getitem(x, i-j)
+            for j, b in enumerate(self.b):
+                nextValue += b * getitem(y, i-1-j)
+            y.append(nextValue)
+
+        return y
+
+    @classmethod
+    def lowPass(cls, fc):
+        x = exp(-2*pi*fc)
+        return cls([1-x], [x])
--- sixtyeight/test/test_mathutil.py
+++ sixtyeight/test/test_mathutil.py
@@ -27,3 +27,35 @@ class TestMovingSum(TestCase):
         r = range(5)
         s = mathutil.movingSum(r, 5)
         self.assertEqual(s, [sum(r)])
+
+
+class TestIIR(TestCase):
+    def test_identity(self):
+        f = mathutil.IIR([1], [0])
+        x = range(5)
+        y = f.filter(x)
+        self.assertEqual(x, y)
+
+    def test_init(self):
+        a = [2, 3]
+        b = [4, 5]
+        f = mathutil.IIR(a, b)
+        self.assertEqual(f.a, a)
+        self.assertEqual(f.b, b)
+
+        x = range(4)
+        y = f.filter(x)
+
+        self.assertEqual(len(x), len(y))
+
+        i = 0
+        self.assertEqual(y[i], a[0]*x[i] + 0           + 0           + 0          )
+
+        i = 1
+        self.assertEqual(y[i], a[0]*x[i] + a[1]*x[i-1] + b[0]*y[i-1] + 0          )
+
+        i = 2
+        self.assertEqual(y[i], a[0]*x[i] + a[1]*x[i-1] + b[0]*y[i-1] + b[1]*y[i-2])
+
+        i = 3
+        self.assertEqual(y[i], a[0]*x[i] + a[1]*x[i-1] + b[0]*y[i-1] + b[1]*y[i-2])
